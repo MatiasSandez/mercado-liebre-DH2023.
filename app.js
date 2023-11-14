e7d5de1f5a42f4677142ad04e6d661f91aacb23d
Matias Sandez <fxs.deagle@gmail.com>
2023-11-13 21:15:34 -0300

    tercer comit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.listen(port,() => console.log("Servidor"))
 console.log("Servidor corriendo")
 
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
 
